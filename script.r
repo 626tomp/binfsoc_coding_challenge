fa11c3be51714adec729c3ffdef0f4963e552276
Tom Parish <1tomparish@gmail.com>
2022-07-05 15:41:35 +1000

    r scripting

---
+++ script.r
@@ -0,0 +1,25 @@
+if (!require("BiocManager", quietly = TRUE))
+  install.packages("BiocManager")
+
+BiocManager::install(version = "3.15")
+BiocManager::install("edgeR")
+BiocManager::install("tximport")
+
+# https://bioconductor.org/packages/release/bioc/vignettes/tximport/inst/doc/tximport.html
+
+library(tximport)
+library(tximportData)
+
+
+# haven't been tested/run, from link above
+
+samples <- read.table(file.path("samples.txt"), header = TRUE)
+samples
+
+files <- file.path(dir, "salmon", samples$run, "results/quant.sf")
+
+names(files) <- paste0("sample", 1)
+
+txi.salmon <- tximport(files, type = "salmon", tx2gene = tx2gene)
+
+head(txi.salmon$counts)
