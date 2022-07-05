6f9ce01cf3b3894842febdeebb7992760faf5f74
Tom Parish <tomp@pop-os.localdomain>
2022-07-05 10:01:47 +1000

    added results from salmon

--- process.sh
+++ process.sh
@@ -1,12 +1,16 @@
 # https://combine-lab.github.io/salmon/getting_started/
 
+#conda activate salmon
 #salmon index -t GRCh38.all.fa.gz -i genome_index
 
 for f in *.fastq.gz
 do
     echo "Processing $f"
-    filename= echo $f | cut -d'.' -f1
-    #salmon quant -i genome_index -l A -r SRR13783589.fastq.gz -p 8 --validateMappings -o quants/$filename
+    filename=$(echo $f | cut -d'.' -f1)
+    echo "Running: salmon quant -i genome_index -l A -r $f -p 8 --validateMappings -o quants/${filename}/"
+    #salmon quant -i genome_index -l A -r $f -p 8 --validateMappings -o quants/${filename}/
+    
+    $(cp quants/${filename}/quant.sf  results/${filename}.sf)
 
 done
 
