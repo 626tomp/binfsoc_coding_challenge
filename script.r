79c08c78e5d25fde95498804d08df7df7a7d5782
Tom Parish <1tomparish@gmail.com>
2022-07-05 18:00:11 +1000

    r script working

--- script.r
+++ script.r
@@ -1,40 +1,26 @@
 if (!require("BiocManager", quietly = TRUE))
   install.packages("BiocManager")
 
+install.packages("readr")
+
 BiocManager::install(version = "3.15")
 BiocManager::install("edgeR")
 BiocManager::install("tximport")
-BiocManager::install("ensembldb")
-BiocManager::install("AnnotationDbi")
-BiocManager::install("TxDb.Hsapiens.UCSC.hg38.knownGene")
 
-
-# https://bioconductor.org/packages/release/bioc/vignettes/tximport/inst/doc/tximport.html
 
 library(tximport)
-library(tximportData)
 
+# get data
+files <- file.path("results", list.files("./results"))
 
-# haven't been tested/run, from link above
-
-
-# gets names of samples from samples.txt
-samples <- read.table(file.path("samples.txt"), header = TRUE)
-samples
-
-files <- file.path(samples, "quant.sf")
-
+# name samples
 names(files) <- paste0("sample", 1:6)
 
-
-library(TxDb.Hsapiens.UCSC.hg38.knownGene)
-txdb <- TxDb.Hsapiens.UCSC.hg38.knownGene
-
-k <- keys(txdb, keytype = "TXNAME")
-tx2gene <- select(txdb, k, "GENEID", "TXNAME")
+#get list of gene names
+tx2gene <- read.table(file.path("tx2gene.txt"))
 
 
 library(tximport)
 txi <- tximport(files, type = "salmon", tx2gene = tx2gene)
-names(txi)
+  names(txi)
 head(txi$counts)
